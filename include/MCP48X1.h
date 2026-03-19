723f631265794f21a8f4a2f636b2a34f802a2c55
Bammer187 <bammer.lukas@gmail.com>
2026-03-19 20:10:36 +0100

    Created mcp48x1_t struct

--- include/MCP48X1.h
+++ include/MCP48X1.h
@@ -1,1 +1,21 @@
-void func(void);
+/*
+ * SPDX-FileCopyrightText: 2026 Lukas Bammer
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef MCP48X1_H
+#define MCP48X1_H
+
+typedef enum {
+	MCP4801,
+	MCP4811,
+	MCP4821
+}mcp48x1_type_t;
+
+typedef struct {
+	spi_device_handle_t spi_handle;
+	mcp48x1_type_t model;
+}mcp48x1_t;
+
+#endif
